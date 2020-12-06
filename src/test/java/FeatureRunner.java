b39ef68363b028a3268ef9a2be51db71ed6e29b3
tiger1911 <mehmetatas@yahoo.com>
2020-12-06 21:56:46 +0100

    Karate first day added

--- src/test/java/FeatureRunner.java
+++ src/test/java/FeatureRunner.java
@@ -1,7 +1,11 @@
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
+@KarateOptions(
+        tags = "@wip"
+)
 public class FeatureRunner {
 
 
